f4171c498857e5befcab0e7fa78db9fdb5593b4e
Ryan Araujo <ryan.pablo.silva.araujo@gmail.com>
2024-06-08 10:02:18 -0300

    feat(ui): test graphics libraries integration

--- js/script.js
+++ js/script.js
@@ -1,3 +1,94 @@
 
-const weatherData = []
+const weatherData = fetchWeatherData()
 
+async function fetchWeatherData() {
+    try {
+        apiUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ITAMBE%20BA?unitGroup=metric&include=days%2Chours&key=8XNTXJF3VUFF3A47D4GHAGP54&contentType=json'
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Erro ao buscar os dados da API');
+        }
+        const data = await response.json();
+        renderAPIinInterface(data)
+        renderGraficoWithMaxMinTemp(data.days)
+        
+    } catch (error) {
+        console.error('Erro:', error);
+    }
+}
+
+function formatDate(dateStr) {
+    const date = new Date(dateStr);
+    return date.toLocaleDateString('pt-BR');
+}
+
+function renderAPIinInterface(weatherData) {
+    const today = new Date().toISOString().split('T')[0];
+    const currentDay = weatherData.days.find(day => day.datetime === today);
+    document.getElementById('current-temp').innerHTML = `Média de Temperatura de Hoje: <b>${currentDay.temp}°C</b>`;
+
+
+    const tableBody = document.getElementById('weather-table').querySelector('tbody');
+    weatherData.days.forEach(day => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${formatDate(day.datetime)}</td>
+            <td>${day.tempmax}°C</td>
+            <td>${day.tempmin}°C</td>
+            <td>${day.temp}°C</td>
+        `;
+        tableBody.appendChild(row);
+    });
+}
+
+
+function renderGraficoWithMaxMinTemp(data) {
+    // Data retrieved https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature
+    // Lista para armazenar os valores de "datetime"
+    const dates = [];
+    const listOfTempMin = [];
+    const listOfTempMax = [];
+
+    // Iterar sobre cada objeto da lista de dados
+    data.forEach(obj => {
+        // Extrair o valor do dia
+        const day = obj.datetime.slice(-2);
+        // Adicionar o valor do dia à lista
+        dates.push(day);
+        listOfTempMin.push(obj.tempmin)
+        listOfTempMax.push(obj.tempmax)
+    });
+
+Highcharts.chart('container', {
+    chart: {
+        type: 'line'
+    },
+    title: {
+        text: 'Mínima e Máxima dos Próximos 15 dias'
+    },
+    xAxis: {
+        categories: dates
+    },
+    yAxis: {
+        title: {
+            text: 'Temperature (°C)'
+        }
+    },
+    plotOptions: {
+        line: {
+            dataLabels: {
+                enabled: true
+            },
+            enableMouseTracking: false
+        }
+    },
+    series: [{
+        name: 'Max',
+        data: listOfTempMax
+    }, {
+        name: 'Min',
+        data: listOfTempMin
+    }]
+});
+
+}
