25433e3403c50a68e1f879c218cffc50def016c2
Ryan Araujo <ryan.pablo.silva.araujo@gmail.com>
2024-06-08 15:29:20 -0300

    feat: Add today's weather information

--- js/script.js
+++ js/script.js
@@ -1,94 +1,98 @@
-
-const weatherData = fetchWeatherData()
-
 async function fetchWeatherData() {
-    try {
-        apiUrl = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ITAMBE%20BA?unitGroup=metric&include=days%2Chours&key=8XNTXJF3VUFF3A47D4GHAGP54&contentType=json'
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error('Erro ao buscar os dados da API');
-        }
-        const data = await response.json();
-        renderAPIinInterface(data)
-        renderGraficoWithMaxMinTemp(data.days)
-        
-    } catch (error) {
-        console.error('Erro:', error);
+  try {
+    apiUrl =
+      "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/ITAMBE%20BA?unitGroup=metric&include=days%2Chours&key=8XNTXJF3VUFF3A47D4GHAGP54&contentType=json";
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error("Erro ao buscar os dados da API");
     }
+    const data = await response.json();
+    renderAPIinInterface(data);
+    renderGraphWithMaxMinTemp(data.days);
+  } catch (error) {
+    console.error("Erro:", error);
+  }
 }
 
 function formatDate(dateStr) {
-    const date = new Date(dateStr);
-    return date.toLocaleDateString('pt-BR');
+  const date = new Date(dateStr);
+  return date.toLocaleDateString('pt-BR');
 }
 
 function renderAPIinInterface(weatherData) {
-    const today = new Date().toISOString().split('T')[0];
-    const currentDay = weatherData.days.find(day => day.datetime === today);
-    document.getElementById('current-temp').innerHTML = `Média de Temperatura de Hoje: <b>${currentDay.temp}°C</b>`;
+  const today = new Date().toISOString().split("T")[0];
+  const currentDay = weatherData.days.find((day) => day.datetime === today);
+  document.getElementById("temp_value").innerHTML = `${currentDay.temp} Cº`
+  document.getElementById("umid_value").innerHTML = `${currentDay.humidity} %`
+  document.getElementById("condi_value").innerHTML = currentDay.conditions
+  document.getElementById("txtTitlePrimary").innerHTML = `Previsão  do Tempo em ${weatherData.resolvedAddress}`
 
-
-    const tableBody = document.getElementById('weather-table').querySelector('tbody');
-    weatherData.days.forEach(day => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
+  const tableBody = document
+    .getElementById("weather-table")
+    .querySelector("tbody");
+  weatherData.days.forEach((day) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
             <td>${formatDate(day.datetime)}</td>
             <td>${day.tempmax}°C</td>
             <td>${day.tempmin}°C</td>
             <td>${day.temp}°C</td>
         `;
-        tableBody.appendChild(row);
-    });
+    tableBody.appendChild(row);
+  });
 }
 
+function renderGraphWithMaxMinTemp(data) {
+  // Data retrieved https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature
+  // Lista para armazenar os valores de "datetime"
+  const dates = [];
+  const listOfTempMin = [];
+  const listOfTempMax = [];
 
-function renderGraficoWithMaxMinTemp(data) {
-    // Data retrieved https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature
-    // Lista para armazenar os valores de "datetime"
-    const dates = [];
-    const listOfTempMin = [];
-    const listOfTempMax = [];
+  // Iterar sobre cada objeto da lista de dados
+  data.forEach((obj) => {
+    // Extrair o valor do dia
+    const day = obj.datetime.slice(-2);
+    // Adicionar o valor do dia à lista
+    dates.push(day);
+    listOfTempMin.push(obj.tempmin);
+    listOfTempMax.push(obj.tempmax);
+  });
 
-    // Iterar sobre cada objeto da lista de dados
-    data.forEach(obj => {
-        // Extrair o valor do dia
-        const day = obj.datetime.slice(-2);
-        // Adicionar o valor do dia à lista
-        dates.push(day);
-        listOfTempMin.push(obj.tempmin)
-        listOfTempMax.push(obj.tempmax)
-    });
-
-Highcharts.chart('container', {
+  Highcharts.chart("container", {
     chart: {
-        type: 'line'
+      type: "line",
     },
     title: {
-        text: 'Mínima e Máxima dos Próximos 15 dias'
+      text: "Mínima e Máxima dos Próximos 15 dias",
     },
     xAxis: {
-        categories: dates
+      categories: dates,
     },
     yAxis: {
-        title: {
-            text: 'Temperature (°C)'
-        }
+      title: {
+        text: "Temperature (°C)",
+      },
     },
     plotOptions: {
-        line: {
-            dataLabels: {
-                enabled: true
-            },
-            enableMouseTracking: false
-        }
+      line: {
+        dataLabels: {
+          enabled: true,
+        },
+        enableMouseTracking: false,
+      },
     },
-    series: [{
-        name: 'Max',
-        data: listOfTempMax
-    }, {
-        name: 'Min',
-        data: listOfTempMin
-    }]
-});
-
+    series: [
+      {
+        name: "Max",
+        data: listOfTempMax,
+      },
+      {
+        name: "Min",
+        data: listOfTempMin,
+      },
+    ],
+  });
 }
+
+fetchWeatherData();
